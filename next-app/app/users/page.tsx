8a199186e5d222fe84a065c8258cfb333375a306
Jackie <jackie15910@gmail.com>
2024-01-25 18:37:44 -0500

    Added Users JSON list

--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const UsersPage = () => {
-  const res = await fetch('http://jsonplaceholder.typicode.com/users')
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch('http://jsonplaceholder.typicode.com/users'); //res is the response
+  const users: User[] = await res.json(); //uses the typescript to identify each property and translates it to json format
   return (
-    <div>UsersPage</div>
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
   )
 }
 
